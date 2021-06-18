51b0494a938d16b5cb718abbeaffe538b0e5c2c1
Rajat Patel <18bce191@nirmauni.ac.in>
2021-06-18 13:08:13 +0530

    Implemented list owner functionality. closes #16

--- pet-clinic-web/src/main/java/rajat/spring/petclinicspring/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/rajat/spring/petclinicspring/controllers/OwnerController.java
@@ -1,15 +1,24 @@
 package rajat.spring.petclinicspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rajat.spring.petclinicspring.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
