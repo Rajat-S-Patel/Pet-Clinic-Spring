72d5c6bea11c2f976b2bcfa10536f03b3ce7188e
Rajat Patel <18bce191@nirmauni.ac.in>
2021-06-21 00:11:04 +0530

    Added Specialties Map Service implementation. closes #31

---
+++ pet-clinic-data/src/main/java/rajat/spring/petclinicspring/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package rajat.spring.petclinicspring.services;
+
+import rajat.spring.petclinicspring.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
