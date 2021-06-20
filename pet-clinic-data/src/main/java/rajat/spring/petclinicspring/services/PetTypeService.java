54a6733916d8fed2d959abfdd493a79122f0e36d
Rajat Patel <18bce191@nirmauni.ac.in>
2021-06-20 21:53:16 +0530

    Added PetType Map service implementation. closes #29

---
+++ pet-clinic-data/src/main/java/rajat/spring/petclinicspring/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package rajat.spring.petclinicspring.services;
+
+import rajat.spring.petclinicspring.model.PetType;
+import rajat.spring.petclinicspring.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
