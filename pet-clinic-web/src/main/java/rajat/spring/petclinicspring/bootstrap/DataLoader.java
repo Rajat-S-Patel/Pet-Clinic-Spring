ac2ac0d3f6ad884fbf3d20e9b4011b786e47afdf
Rajat Patel <18bce191@nirmauni.ac.in>
2021-06-20 22:10:08 +0530

    Added PetType in Bootstrap. closes #32

--- pet-clinic-web/src/main/java/rajat/spring/petclinicspring/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/rajat/spring/petclinicspring/bootstrap/DataLoader.java
@@ -3,25 +3,43 @@ package rajat.spring.petclinicspring.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import rajat.spring.petclinicspring.model.Owner;
+import rajat.spring.petclinicspring.model.PetType;
 import rajat.spring.petclinicspring.model.Vet;
 import rajat.spring.petclinicspring.services.OwnerService;
+import rajat.spring.petclinicspring.services.PetTypeService;
 import rajat.spring.petclinicspring.services.VetService;
 import rajat.spring.petclinicspring.services.map.OwnerServiceMap;
 import rajat.spring.petclinicspring.services.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
+
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
+
         System.out.println("Inside dataloader");
+
+        PetType dog = new PetType();
+        dog.setName("Max");
+        petTypeService.save(dog);
+        System.out.println("saved dog with id: "+dog.getId());
+
+        PetType cat = new PetType();
+        cat.setName("Mini");
+        petTypeService.save(cat);
+        System.out.println("saved cat with id: "+cat.getId());
+
         System.out.println("Creating owners");
 
         Owner owner1 = new Owner();
