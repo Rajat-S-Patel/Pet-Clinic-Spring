2619b939210dcefbe1a07da52e924aa9e38731c1
Rajat Patel <18bce191@nirmauni.ac.in>
2021-06-21 01:03:40 +0530

    Created specialties and added to Vets on startup. closes #34

--- pet-clinic-data/src/main/java/rajat/spring/petclinicspring/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/rajat/spring/petclinicspring/services/map/VetServiceMap.java
@@ -1,13 +1,21 @@
 package rajat.spring.petclinicspring.services.map;
 
 import org.springframework.stereotype.Service;
+import rajat.spring.petclinicspring.model.Speciality;
 import rajat.spring.petclinicspring.model.Vet;
+import rajat.spring.petclinicspring.services.SpecialtiesService;
 import rajat.spring.petclinicspring.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
+    private final SpecialtiesService specialtiesService;
+
+    public VetServiceMap(SpecialtiesService specialtiesService) {
+        this.specialtiesService = specialtiesService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -25,6 +33,15 @@ public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetSe
 
     @Override
     public Vet save(Vet object) {
+        if(object == null) return null;
+        if(!object.getSpecialities().isEmpty()){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpecialty=specialtiesService.save(speciality);
+                    speciality.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/rajat/spring/petclinicspring/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/rajat/spring/petclinicspring/bootstrap/DataLoader.java
@@ -2,12 +2,10 @@ package rajat.spring.petclinicspring.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import rajat.spring.petclinicspring.model.Owner;
-import rajat.spring.petclinicspring.model.Pet;
-import rajat.spring.petclinicspring.model.PetType;
-import rajat.spring.petclinicspring.model.Vet;
+import rajat.spring.petclinicspring.model.*;
 import rajat.spring.petclinicspring.services.OwnerService;
 import rajat.spring.petclinicspring.services.PetTypeService;
+import rajat.spring.petclinicspring.services.SpecialtiesService;
 import rajat.spring.petclinicspring.services.VetService;
 
 import java.time.LocalDate;
@@ -18,11 +16,13 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtiesService specialtiesService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtiesService specialtiesService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtiesService = specialtiesService;
     }
 
 
@@ -30,7 +30,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         System.out.println("Inside dataloader");
+        if(petTypeService.findAll().size() == 0)
+            loadData();
+    }
 
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogType=petTypeService.save(dog);
@@ -77,14 +81,28 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Creating Vets");
 
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology=specialtiesService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery=specialtiesService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry=specialtiesService.save(dentistry);
+
         Vet vet1 = new Vet();
         vet1.setFirstName("Rajan");
         vet1.setLastName("Patel");
+        vet1.getSpecialities().add(savedDentistry);
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Shraddha");
         vet2.setLastName("Patel");
+        vet2.getSpecialities().add(savedSurgery);
         vetService.save(vet2);
 
         System.out.println("Done....");
